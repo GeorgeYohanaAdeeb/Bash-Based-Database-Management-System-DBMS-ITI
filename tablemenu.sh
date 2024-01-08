1a036cacc00d365f084fd775655cc3bea60c4415
George Yohana Adeeb <107894750+GeorgeYohanaAdeeb@users.noreply.github.com>
2024-01-08 11:20:03 +0200

    First Commit
    George Yohana Adeeb
    https://www.linkedin.com/in/george-yohana-adeeb/
    
    Rofaida Belal Abdullah
    https://www.linkedin.com/in/rofidabelal/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
    
    Co-Authored-By: Rofaida Belal <155958446+rofaida1@users.noreply.github.com>

---
+++ tablemenu.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/bash
+
+choice=$(whiptail --title "Table Menu" --menu "select option:" 20 60 0\
+                                "1" "Create Table" \
+                                "2" "List Tables" \
+                                "3" "Drop Table" \
+                                "4" "Insert Into Table" \
+                                "5" "Select from Table" \
+                                "6" "Delete from Table" \
+                                "7" "Update Table" \
+                                "8" "Back to Main Menu" 3>&1 1>&2 2>&3)
+
+case $choice in 
+1) 
+. ~/finaltable.sh
+;;
+2) 
+. ~/listtable.sh
+;;
+3)
+. ~/droptable.sh
+;;
+4) 
+. ~/insert.sh
+;; 
+5)
+. ~/select.sh
+;;
+6)
+. ~/delete.sh
+;;
+7)
+. ~/update.sh
+;;
+8) 
+. ~/project.sh
+;;
+*)
+. ~/project.sh
+;;
+esac
+. ~/tablemenu.sh
+
+
